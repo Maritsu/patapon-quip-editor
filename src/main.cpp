0f617f16e39b9346ddf07463433986193b3d21d1
Maritsu <66056347+Maritsu@users.noreply.github.com>
2023-11-10 19:12:13 +0100

    [ui] make dynamic elements disabled by default

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
-#include <QtGui>
 #include <QApplication>
 #include <QLabel>
+#include <QtGui>
 
-int main(int argc, char* argv[]) {
-	QApplication app(argc, argv);
-	QLabel label("Hello, world!");
-	label.show();
-	return app.exec();
+int main(int argc, char *argv[]) {
+  QApplication app(argc, argv);
+  QLabel label("Hello, world!");
+  label.show();
+  return app.exec();
 }
 
 // p3qe
