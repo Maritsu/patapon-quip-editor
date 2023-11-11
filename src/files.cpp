82c5c9edfe04d860ababb057068fd04b91dc5986
Maritsu <66056347+Maritsu@users.noreply.github.com>
2023-11-11 16:09:53 +0100

    [files] add load and save functions

--- src/files.cpp
+++ src/files.cpp
@@ -6,10 +6,47 @@ QString execPathDialog(QWidget *parent) {
 }
 
 QByteArray loadData(const QString &path, bool pspEncrypted, bool p3Encrypted) {
+  QFile file(path);
+  if (!file.open(QFile::ReadOnly)) {
+    throw std::runtime_error("Could not read input file!");
+  } else {
+    QByteArray data = file.readAll();
+    if (pspEncrypted) {
+    } // not implemented
+    if (p3Encrypted) {
+    } // not implemented
+    file.close();
+    return data;
+  }
 }
 
 bool saveData(QFile *file, const QByteArray &newData, bool backup,
               bool pspEncrypt, bool p3Encrypt) {
+  QSaveFile saveFile = QSaveFile(file->fileName());
+  if (backup) {
+  } // not implemented
+  if (!saveFile.open(QFile::WriteOnly)) {
+    throw std::runtime_error("Could not open output file for writing!");
+  }
+  QByteArray data = newData;
+  if (p3Encrypt) {
+  } // not implemented
+  if (pspEncrypt) {
+  } // not implemented
+  if (qint64 bytesWritten;
+      (bytesWritten = saveFile.write(data)) != data.size()) {
+    if (bytesWritten == -1) {
+      saveFile.cancelWriting();
+      throw std::runtime_error(
+          "An error occured while writing data (-1 returned)");
+    } else /* 0 <= bytesWritten < newData.size() */ {
+      saveFile.cancelWriting();
+      throw std::runtime_error("Data write failed (" +
+                               std::to_string(bytesWritten) + ") returned)");
+    }
+  }
+  saveFile.commit();
+  return true;
 }
 
 QByteArray decrypt(const QByteArray &data) {
